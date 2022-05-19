8abf8e80ca4c814a21c661780a8c4cdd4541089f
James Stillman <jamesstillman99@gmail.com>
2022-05-19 09:46:50 +0100

    Set up project structure

---
+++ src/test/java/com/example/repository/ComposerRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+import com.example.model.Composer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComposerRepository extends JpaRepository<Composer, Long> {
+}
