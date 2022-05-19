f10bc733872d1bb413a2f0658d7523563fa18518
James Stillman <jamesstillman99@gmail.com>
2022-05-19 12:36:12 +0100

    Added delete mapping to Composer

--- src/main/java/com/example/spring_solo_project/controller/ComposerController.java
+++ src/main/java/com/example/spring_solo_project/controller/ComposerController.java
@@ -2,7 +2,9 @@ package com.example.spring_solo_project.controller;
 
 import com.example.spring_solo_project.model.Composer;
 import com.example.spring_solo_project.repository.ComposerRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +22,11 @@ public class ComposerController {
     @GetMapping("/composers")
     public List<Composer> getComposers(){
         return composerRepository.findAll();
+    }
+
+    @DeleteMapping("/composers/delete/{id}")
+    public void deleteComposerById(@PathVariable Long id){
+        composerRepository.deleteById(id);
     }
 
 }
--- src/test/java/com/example/spring_solo_project/SpringSoloProjectApplicationTests.java
+++ src/test/java/com/example/spring_solo_project/SpringSoloProjectApplicationTests.java
@@ -1,13 +1,78 @@
 package com.example.spring_solo_project;
 
+import com.example.spring_solo_project.model.Composer;
+import com.example.spring_solo_project.model.Music;
+import com.example.spring_solo_project.repository.ComposerRepository;
+import com.example.spring_solo_project.repository.MusicRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SpringSoloProjectApplicationTests {
+
+	@Autowired
+	MusicRepository musicRepository;
+
+	@Autowired
+	ComposerRepository composerRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void dataSqlLoadsMusic(){
+		List<Music> found = musicRepository.findAll();
+		assertEquals(243,found.size());
+	}
+
+	@Test
+	public void dataSqlLoadsComposers(){
+		List<Composer> found = composerRepository.findAll();
+		assertEquals(50, found.size());
+	}
+
+	@Test
+	public void canFindMusicByTitle(){
+		Music found = musicRepository.findByTitle("Fortress 2");
+		assertEquals("Fortress 2", found.getTitle());
+	}
+
+	@Test
+	public void canFindMusicByTitle_nonExistentTitle(){
+		Music found = musicRepository.findByTitle("arenfaoiernfioanio");
+		assertNull(found);
+	}
+
+	@Test
+	public void canFilterByYear(){
+		List<Music> found = musicRepository.findAllComposedAfterYear(2000);
+		assertEquals(9, found.size());
+	}
+
+	@Test
+	public void canFilterByYear_givenFuture(){
+		List<Music> found = musicRepository.findAllComposedAfterYear(2100);
+		assertTrue(found.isEmpty());
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
