696beee269ecf5a7a73cf32eb8c46c5a2b3c8dee
James Stillman <jamesstillman99@gmail.com>
2022-05-19 10:11:12 +0100

    Implemented Controllers

--- src/main/java/com/example/spring_solo_project/controller/ComposerController.java
+++ src/main/java/com/example/spring_solo_project/controller/ComposerController.java
@@ -1,4 +1,25 @@
 package com.example.spring_solo_project.controller;
 
+import com.example.spring_solo_project.model.Composer;
+import com.example.spring_solo_project.repository.ComposerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ComposerController {
+
+
+    private final ComposerRepository composerRepository;
+
+    public ComposerController(ComposerRepository composerRepository){
+        this.composerRepository = composerRepository;
+    }
+
+    @GetMapping("/composers")
+    public List<Composer> getComposers(){
+        return composerRepository.findAll();
+    }
+
 }
--- src/main/java/com/example/spring_solo_project/controller/MusicController.java
+++ src/main/java/com/example/spring_solo_project/controller/MusicController.java
@@ -1,4 +1,24 @@
 package com.example.spring_solo_project.controller;
 
+import com.example.spring_solo_project.model.Music;
+import com.example.spring_solo_project.repository.MusicRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MusicController {
+
+    private final MusicRepository musicRepository;
+    public MusicController(MusicRepository musicRepository) {
+        this.musicRepository = musicRepository;
+    }
+
+    @GetMapping("/music")
+    public List<Music> getMusic(){
+        return musicRepository.findAll();
+    }
+
+
 }
